b18c003cb5a4a33db9e02a9dc233e25f03fa8f3a
Houda <houda.riyad-etu@etu.univh2c.ma>
2026-05-05 19:30:38 +0100

    Finalisation de l'application

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -1,13 +1,27 @@
 import { TestBed } from '@angular/core/testing';
-
+import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClientTesting, HttpTestingController } from '@angular/common/http/testing';
 import { ProductService } from './product.service';
 
 describe('ProductService', () => {
   let service: ProductService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        ProductService,
+        provideHttpClient(),
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(ProductService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    // Verifies that no strictly unexpected requests are outstanding
+    httpMock.verify();
   });
 
   it('should be created', () => {
--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  constructor(private HttpClient: any) {}
+  // Constructor uses lowercase 'http' to match method calls
+  constructor(private http: HttpClient) {}
+
   getAllproducts(): Observable<any> {
     return this.http.get('http://localhost:8083/products');
   }
-  deleteProduct(product: any) {
+
+  deleteProduct(product: any): Observable<any> {
     return this.http.delete('http://localhost:8083/products/' + product.id);
   }
 }
