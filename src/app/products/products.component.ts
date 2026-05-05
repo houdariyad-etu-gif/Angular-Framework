e7aeceb2d7a4c53a35e161e2018669e02038f5bf
Houda <houda.riyad-etu@etu.univh2c.ma>
2026-05-05 01:57:15 +0100

    Ajout de la table des produits}

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-products',
   imports: [],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css',
+  standalone: true
 })
-export class ProductsComponent {}
+export class ProductsComponent implements OnInit {
+  products! : Array<any>;
+
+  constructor() {}
+
+  ngOnInit() {
+    this.products = [
+      { id: 1, name: 'Computer', price: 2300, selected: true },
+      { id: 2, name: 'Printer', price: 1200, selected: false },
+      { id: 3, name: 'Smart Phone', price: 1100, selected: true },
+    ]
+  }
+}
